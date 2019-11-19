bf992f823262d3ee04f07057b21aa5ca70a3c54b
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-19 10:18:25 +0200

    String methods finally updated

--- StringMethods.java
+++ StringMethods.java
@@ -1,8 +1,8 @@
 public class StringMethods{
-	public static boolean appearsIn(char x, String y){
+	public static boolean appearsIn(char c, String text){
 
 	}
-	public static int findOccurences(char x, String y){
+	public static int findOccurences(char c, String text){
 
 	}
 	public static void displayManyTimes(int n1, String a){
