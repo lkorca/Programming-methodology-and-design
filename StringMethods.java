8fda2804d2e3755a1699ce5bb357887e9381aba9
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-19 09:53:26 +0200

    String Methods file

---
+++ StringMethods.java
@@ -0,0 +1,11 @@
+public class StringMethods{
+	public static char appearsIn(char x, String y){
+
+	}
+	public static int findOccurences(char x, String y){
+
+	}
+	public static void displayManyTimes(int n1, String a){
+		
+	}
+}
