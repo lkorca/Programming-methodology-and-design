5342a97ab23213fc75708f4442e05a0d079c6ece
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-19 09:37:18 +0200

    Updated file

--- NumberMethods.java
+++ NumberMethods.java
@@ -1,3 +1,35 @@
 public class NumberMethods{
-	
+	public static int minimumInt(int n1, int n2){
+
+	}
+	public static int maximumInt(int n1, int n2){
+
+	}
+	public static int minimumDouble(double n1, double n2){
+
+	}
+	public static int maximumDouble(double n1, double n2){
+
+	}
+	public static int median(int n1, int n2, int n3){
+
+	}
+	public static int average(int n1, int n2, int n3, int n4, int n5){
+
+	}
+	public static boolean isEven(int n1){
+
+	}
+	public static int sumAllNumbersUpTo(int n5){
+
+	}
+	public static int sumEvenNumbersUpTo(int n5){
+		
+	}
+	public static double percentage(int n1, int n2){
+		
+	}
+	public static double solveEquatiom(double n1, double n2){
+		
+	}
 }
