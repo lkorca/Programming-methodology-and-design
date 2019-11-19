c816a33768e5a0f8bc1ce6bbfcebc39295a061d9
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-19 10:36:27 +0200

    Grade Methods new file

--- GradeMethods.java
+++ GradeMethods.java
@@ -1,8 +1,17 @@
 public class GradeMethods{
-	public static void promptForGrade(double g1){
+	public static int promptForGrade(){
 
 	}
 	public static boolean isPassGrade(double g1){
+
+	}
+	public static boolean isFirstClass(double g1){
+		
+	}
+	public static String findClassification(double g1){
+		
+	}
+	public static double findClassification(double g1, double g2){
 		
 	}
 }
