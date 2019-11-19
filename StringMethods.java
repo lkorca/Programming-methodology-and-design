9a544db285ccea2c901f95e80db835087c74693a
lkorca <lkorca@citycollege.sheffield.eu>
2019-11-19 10:10:22 +0200

    String methods updated file

--- StringMethods.java
+++ StringMethods.java
@@ -1,11 +1,11 @@
 public class StringMethods{
-	public static char appearsIn(char x, String y){
+	public static boolean appearsIn(char x, String y){
 
 	}
 	public static int findOccurences(char x, String y){
 
 	}
 	public static void displayManyTimes(int n1, String a){
-		
+
 	}
 }
